93767c453d3202d6cbb14c172f1d061372a2ac75
Marcelo Luza <marcelo@siliconvillage.dev>
2024-12-08 21:24:35 +0000

    ajuste comentarios

--- src/app/services/lista.service.ts
+++ src/app/services/lista.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ListaService {
-  private baseUrl = 'http://localhost:3000/api/lists'; // Altere conforme sua API
+  private baseUrl = 'http://localhost:3000/api/lists';
 
   constructor(private http: HttpClient) {}
 
